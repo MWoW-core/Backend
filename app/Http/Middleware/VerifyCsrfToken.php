0244e6ad8072891bfa672f2c54cc90f67bc451bd
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2020-05-03 16:01:27 +0200

    [Temp fix]: Added auth endpoints to CSRF exceptions...

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -20,5 +20,8 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/webhooks/github/deploy',
+        '/login',
+        '/register',
+        '/logout'
     ];
 }
